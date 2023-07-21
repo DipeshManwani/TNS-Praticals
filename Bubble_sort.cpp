11844524a40ef4731d8d5bb4b8449b1ecf7f658a
DipeshManwani <100461773+DipeshManwani@users.noreply.github.com>
2023-07-21 16:46:47 +0530

    Add files via upload

---
+++ Bubble_sort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, i, arr[5], j, demo;
+    cout<<"Enter the Size (max. 5): ";
+    cin>>n;
+    cout<<"Enter "<<n<<" Numbers: ";
+    for(i=0; i<n; i++)
+        cin>>arr[i];
+    cout<<"\nSorting the Array using Bubble Sort Technique..\n";
+    for(i=0; i<(n-1); i++)
+    {
+        for(j=0; j<(n-i-1); j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                demo = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = demo;
+            }
+        }
+    }
+    cout<<"\nArray Sorted Successfully!\n";
+    cout<<"\nThe New Array is: \n";
+    for(i=0; i<n; i++)
+        cout<<arr[i]<<" ";
+    cout<<endl;
+    return 0;
+}
